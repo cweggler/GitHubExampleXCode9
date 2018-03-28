d18bc1299f8f1e9c4b9e3730bee6bfa7f0abbd7c
Cara Weggler <cweggler@gmail.com>
2018-03-27 19:33:38 -0500

    Initial Commit

---
+++ GitHubExampleXCode9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubExampleXCode9
+//
+//  Created by Cara on 3/27/18.
+//  Copyright © 2018 Cara Weggler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
